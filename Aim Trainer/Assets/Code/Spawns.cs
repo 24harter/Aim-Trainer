e00d8d3239585d0052315e415014d005ea34e5e5
24harter <24harter@gmail.com>
2023-03-07 13:41:52 -0500

    Started work on arrays

---
+++ Aim Trainer/Assets/Code/Spawns.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawns : MonoBehaviour
+{
+	public var q;
+	public var w;
+	public var e;
+	public var r;
+	public var t;
+	public var y;
+	public var u;
+	public var i;
+	public var o;
+	public var p;
+	public var a;
+	public var s;
+	public var d;
+	public var f;
+	public var g;
+	public var h;
+	public var j;
+	public var q;
+	
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public void spawns()
+	{
+		int[] spawn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+
+		spawn[0] = 
+	}
+}
