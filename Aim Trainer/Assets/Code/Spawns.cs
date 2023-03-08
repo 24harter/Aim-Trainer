ed983ca09fea59ac9d9b0ee6c0c7c4eee98bb2e4
24harter <24harter@gmail.com>
2023-03-08 13:39:41 -0500

    work

--- Aim Trainer/Assets/Code/Spawns.cs
+++ Aim Trainer/Assets/Code/Spawns.cs
@@ -4,30 +4,42 @@ using UnityEngine;
 
 public class Spawns : MonoBehaviour
 {
-	public var q;
-	public var w;
-	public var e;
-	public var r;
-	public var t;
-	public var y;
-	public var u;
-	public var i;
-	public var o;
-	public var p;
-	public var a;
-	public var s;
-	public var d;
-	public var f;
-	public var g;
-	public var h;
-	public var j;
-	public var q;
+	public static var q;
+	public static var w;
+	public static var e;
+	public static var r;
+	public static var t;
+	public static var y;
+	public static var u;
+	public static var i;
+	public static var o;
+	public static var p;
+	public static var a;
+	public static var s;
+	public static var d;
+	public static var f;
+	public static var g;
+	public static var h;
+	public static var j;
+	public static var k;
+	public static var l;
+	public static var z;
+	public static var x;
+	public static var c;
+	public static var v;
+	public static var b;
+	public static var n;
+	public static var m;
+	public static var qq;
+	public static var ww;
+	public static var ee;
+	public static var rr;
 	
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(target, spawns.Range(0, 29), spawner.transform.position, spawner.transform.rotation);
     }
 
     // Update is called once per frame
@@ -39,6 +51,35 @@ public class Spawns : MonoBehaviour
 	{
 		int[] spawn = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
-		spawn[0] = 
+		spawn[0] = var q;
+		spawn[1] = var w;
+		spawn[2] = var e;
+		spawn[3] = var r;
+		spawn[4] = var t;
+		spawn[5] = var y;
+		spawn[6] = var u;
+		spawn[7] = var i;
+		spawn[8] = var o;
+		spawn[9] = var p;
+		spawn[10] = var a;
+		spawn[11] = var s;
+		spawn[12] = var d;
+		spawn[13] = var f;
+		spawn[14] = var g;
+		spawn[15] = var h;
+		spawn[16] = var j;
+		spawn[17] = var k;
+		spawn[18] = var l;
+		spawn[19] = var z;
+		spawn[20] = var x;
+		spawn[21] = var c;
+		spawn[22] = var v;
+		spawn[23] = var b;
+		spawn[24] = var n;
+		spawn[25] = var m;
+		spawn[26] = var qq;
+		spawn[27] = var ww;
+		spawn[28] = var ee;
+		spawn[29] = var rr;
 	}
 }
